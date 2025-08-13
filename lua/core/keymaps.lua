4691bc57f4cae78cd5b365c8ec0c9cba90a0f818
Jon Godin <jon.m.godin@gmail.com>
2025-08-13 12:01:36 -0400

    updating text keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -49,20 +49,20 @@ keymap.set("n", "<leader>er", ":NvimTreeFocus<CR>") -- toggle focus to file expl
 keymap.set("n", "<leader>ef", ":NvimTreeFindFile<CR>") -- find file in file explorer
 
 -- Telescope
-keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files({ hidden = true }) end) -- fuzzy find files in project
-keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, {}) -- grep file contents in project
-keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, {}) -- fuzzy find open buffers
-keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, {}) -- fuzzy find help tags
-keymap.set('n', '<leader>fs', require('telescope.builtin').current_buffer_fuzzy_find, {}) -- fuzzy find in current file buffer
-keymap.set('n', '<leader>fo', require('telescope.builtin').lsp_document_symbols, {}) -- fuzzy find LSP/class symbols
-keymap.set('n', '<leader>fi', require('telescope.builtin').lsp_incoming_calls, {}) -- fuzzy find LSP/incoming calls
--- keymap.set('n', '<leader>fm', function() require('telescope.builtin').treesitter({default_text=":method:"}) end) -- fuzzy find methods in current class
-keymap.set('n', '<leader>fm', function() require('telescope.builtin').treesitter({symbols={'function', 'method'}}) end) -- fuzzy find methods in current class
+keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files({ hidden = true }) end, { desc = 'telescope: find in project' }) -- fuzzy find files in project
+keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = 'telescope: live grep' }) -- grep file contents in project
+keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = 'telescope: fuzzy find open buffers' }) -- fuzzy find open buffers
+keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = 'telescope: fuzzy find help tags' }) -- fuzzy find help tags
+keymap.set('n', '<leader>fs', require('telescope.builtin').current_buffer_fuzzy_find, { desc = 'telescope: fuzzy find in current file buffer' }) -- fuzzy find in current file buffer
+keymap.set('n', '<leader>fo', require('telescope.builtin').lsp_document_symbols, { desc = 'telescope: fuzzy find LSP/class symbols' }) -- fuzzy find LSP/class symbols
+keymap.set('n', '<leader>fi', require('telescope.builtin').lsp_incoming_calls, { desc = 'telescope: fuzzy find LSP/incoming calls' }) -- fuzzy find LSP/incoming calls
+keymap.set('n', '<leader>fm', function() require('telescope.builtin').treesitter({symbols={'function', 'method'}}) end, { desc = 'telescope: fuzzy find methods in current class' }) -- fuzzy find methods in current class
 keymap.set('n', '<leader>ft', function() -- grep file contents in current nvim-tree node
   local success, node = pcall(function() return require('nvim-tree.lib').get_node_at_cursor() end)
   if not success or not node then return end;
   require('telescope.builtin').live_grep({search_dirs = {node.absolute_path}})
-end)
+end, { desc = 'telescope: grep file contents in current nvim-tree node' })
+keymap.set('n', '<leader>fk', require('telescope.builtin').keymaps, { desc = 'telescope: fuzzy find in keymappings' }) -- fuzzy find in keymappings
 
 -- Git-blame
 keymap.set("n", "<leader>gb", ":GitBlameToggle<CR>") -- toggle git blame
@@ -153,35 +153,69 @@ keymap.set("n", '<leader>df', '<cmd>Telescope dap frames<cr>')
 keymap.set("n", '<leader>dh', '<cmd>Telescope dap commands<cr>')
 keymap.set("n", '<leader>de', function() require('telescope.builtin').diagnostics({default_text=":E:"}) end)
 
-keymap.set('n', 'zy', '"*y')
-keymap.set('n', 'zu', '"*p')
-keymap.set('n', 'zU', '"*P')
-keymap.set('n', 'zv', 'vi"')
-keymap.set('n', 'zb', 'va"')
-keymap.set('n', 'zc', 'viw')
-keymap.set('n', 'zC', 'viW')
-keymap.set('n', 'zj', '0v')
-keymap.set('n', 'zk', '0v$h')
-keymap.set('n', 'zl', '$h')
-keymap.set('n', 'za', "c''<ESC>P")
-keymap.set('n', 'zs', 'c""<ESC>P')
-keymap.set('n', 'zd', 'c()<ESC>P')
-keymap.set('n', 'zf', 'c{}<ESC>P')
-keymap.set('n', 'zg', 'c<><ESC>P')
-keymap.set('n', 'zi', 'i <ESC>')
-keymap.set('n', 'za', 'a <ESC>')
-keymap.set('n', 'zo', 'o<ESC>')
-keymap.set('n', 'zO', 'O<ESC>')
-keymap.set('n', '<C-[>', '{')
-keymap.set('n', '<C-]>', '}')
+-- -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
 
-keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
-keymap.set('n', '<leader>zs', vim.cmd.Git)
+keymap.set('n', 'zy', '"*y', { desc = 'text: yank to clipboard' })
+keymap.set('n', 'zu', '"*p', { desc = 'text: paste after from clipboard' })
+keymap.set('n', 'zU', '"*P', { desc = 'text: paste before from clipboard' })
+keymap.set('n', 'zj', '0v', { desc = 'text: start of line -> visual mode' })
+keymap.set('n', 'zk', '0v$h', { desc = 'text: select line without CR char' })
+keymap.set('n', 'zl', '$h', { desc = 'text: end of line without CR char' })
+keymap.set('n', 'zi', 'i <ESC>', { desc = 'text: put space before' })
+keymap.set('n', 'za', 'a <ESC>', { desc = 'text: put space after' })
+keymap.set('n', 'zo', 'o<ESC>', { desc = 'text: put line before' })
+keymap.set('n', 'zO', 'O<ESC>', { desc = 'text: put line after' })
+keymap.set('n', '<C-[>', '{', { desc = 'text: jump to previous paragraph' })
+keymap.set('n', '<C-]>', '}', { desc = 'text: jump to next paragraph' })
 
-keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-keymap.set("n", "J", "mzJ`z")
-keymap.set("n", "<C-d>", "<C-d>zz")
-keymap.set("n", "<C-u>", "<C-u>zz")
-keymap.set("n", "n", "nzzzv")
-keymap.set("n", "N", "Nzzzv")
+keymap.set('n', '<leader>ij', 'viW', { desc = 'text: select within non-delimited word' })
+keymap.set('n', '<leader>aj', 'vaW', { desc = 'text: select within non-delimited word' })
+keymap.set('n', '<leader>ik', 'viw', { desc = 'text: select within delimited word' })
+keymap.set('n', '<leader>ak', 'vaw', { desc = 'text: select around delimited word' })
+keymap.set('n', '<leader>il', 'vip', { desc = 'text: select within delimited paragraph' })
+keymap.set('n', '<leader>al', 'vap', { desc = 'text: select around delimited paragraph' })
+keymap.set('n', '<leader>jk', '?[{[(]<CR><ESC>v%', { desc = 'text: select current or previous enclosing' })
+keymap.set('n', '<leader>jj', '/[{[(]<CR><ESC>v%', { desc = 'text: select next enclosing' })
+
+keymap.set('n', '<leader>ia', 'vi\'', { desc = 'text: select within \'' })
+keymap.set('n', '<leader>aa', 'va\'', { desc = 'text: select around \'' })
+keymap.set('v', '<leader>ja', 'c\'\'<ESC>P', { desc = 'text: put \'\' around single line selection' })
+keymap.set('v', '<leader>ka', 'I\'<ESC>gv$<ESC>a\'<ESC>', { desc = 'text: put \'\' around multi line selection' })
+
+keymap.set('n', '<leader>is', 'vi"', { desc = 'text: select within "' })
+keymap.set('n', '<leader>as', 'va"', { desc = 'text: select around "' })
+keymap.set('v', '<leader>js', 'c""<ESC>P', { desc = 'text: put "" around single line selection' })
+keymap.set('v', '<leader>ks', 'I"<ESC>gv$<ESC>a"<ESC>', { desc = 'text: put "" around multi line selection' })
+
+keymap.set('n', '<leader>id', 'vi(', { desc = 'text: select within ()' })
+keymap.set('n', '<leader>ad', 'va(', { desc = 'text: select around ()' })
+keymap.set('v', '<leader>jd', 'c()<ESC>P', { desc = 'text: put () around single line selection' })
+keymap.set('v', '<leader>kd', 'I(<ESC>gv$<ESC>a)<ESC>', { desc = 'text: put () around multi line selection' })
+
+keymap.set('n', '<leader>if', 'vi{', { desc = 'text: select within {}' })
+keymap.set('n', '<leader>af', 'va{', { desc = 'text: select around {}' })
+keymap.set('v', '<leader>jf', 'c{}<ESC>P', { desc = 'text: put {} around single line selection' })
+keymap.set('v', '<leader>kf', 'I{<ESC>gv$<ESC>a}<ESC>', { desc = 'text: put {} around multi line selection' })
+
+keymap.set('n', '<leader>ig', 'vi[', { desc = 'text: select within []' })
+keymap.set('n', '<leader>ag', 'va[', { desc = 'text: select around []' })
+keymap.set('v', '<leader>jg', 'c[]<ESC>P', { desc = 'text: put [] around single line selection' })
+keymap.set('v', '<leader>kg', 'I[<ESC>gv$<ESC>a]<ESC>', { desc = 'text: put [] around multi line selection' })
+
+keymap.set('n', '<leader>ih', 'vi<', { desc = 'text: select within <>' })
+keymap.set('n', '<leader>ah', 'va<', { desc = 'text: select around <>' })
+keymap.set('v', '<leader>jh', 'c<><ESC>P', { desc = 'text: put <> around single line selection' })
+keymap.set('v', '<leader>kh', 'I<<ESC>gv$<ESC>a><ESC>', { desc = 'text: put <> around multi line selection' })
+
+-- -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+--
+keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Undotree: toggle' })
+keymap.set('n', '<leader>zs', vim.cmd.Git, { desc = 'Git: toggle' })
+
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'text: move selection down 1 line' })
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'text: move selection up 1 line' })
+keymap.set("n", "J", "mzJ`z", { desc = 'Git: toggle' })
+keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Git: toggle' })
+keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Git: toggle' })
+keymap.set("n", "n", "nzzzv", { desc = 'Git: toggle' })
+keymap.set("n", "N", "Nzzzv", { desc = 'Git: toggle' })
